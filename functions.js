4714cbf07cc4928a5a79f1e2a834d68bb3e410e8
Vianka Lemus <vianka.lemus@gmail.com>
2016-08-07 10:38:04 -0700

    add another function

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 "use strict"
-
+/*
 let adder = function(a, b) {
 	let name = "123";
 	console.log(name);
@@ -9,3 +9,25 @@ let adder = function(a, b) {
 
 
 console.log(adder(1, 2))
+*/
+
+
+
+
+
+let multi = function(x, y, z) {
+	console.log("called multi");
+	return x *  y * z;
+};
+
+console.log(multi(1, 2, 3))
+
+console.log(multi(5, 2, 3))
+
+let result = multi(5, 3, 6);
+
+console.log(result);
+
+
+
+
