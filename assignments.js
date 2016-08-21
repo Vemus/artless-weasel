5d7ff4a627da30591c21aa716ad0e7de33da8466
Vianka Lemus <vianka.lemus@gmail.com>
2016-08-21 03:13:03 -0700

    tasks 1-3

---
+++ assignments.js
@@ -0,0 +1,111 @@
+// Do these 10 assignments, after each task is correctly completed
+// create a git commit so that you can record your progress.
+
+
+// Task 1
+// Write a function that takes two strings and returns 
+// a new string which is the concatenation of both strings
+
+let sound = function(wave, barrier) {
+	let s = wave + barrier;
+	return s;
+};
+
+
+let pork = function(bacon, ham) {
+let p = bacon + ham;
+return p;
+};
+
+
+
+
+
+
+
+
+let rem = {sleep: 'juice', awake: 'water'}
+
+
+// Task 2
+// Write a function that takes no arguments and gives back 
+// a JavaScript object with the fields foo, bar. Each field
+// can have any value
+
+
+
+let r = function() {
+	let f = {}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let glass = function() {
+	let state = {foo: 'solid', bar: 'liquid'};
+	return state
+};
+
+let something = function() {
+	let anything = {foo: 'this', bar: 'that'};
+	return anything
+};
+
+let clothing = function() {
+	let garments = {foo: 'socks', bar: 'underwear'};
+	return garments
+};
+
+let space = function() {
+	let planet = {foo: 'star', bar: 'moon'};
+	return planet
+};
+
+let hamlet = function() { 
+	let madilene = {foo: 'gor', bar: 'eduard'};
+	return madilene
+};
+
+
+// Task 3 
+// Write a function that takes three arguments and 
+// returns a JavaScript	object with whatever field names 
+// you want, as long as the three field names use the three arguments
+// given to the function
+
+// Task 4
+// Write a function that takes an array of integers as
+// its argument and return the sum of the integers
+
+// Task 5
+// Write a function that takes an array of strings and
+// returns all the strings in the array as one long string.
+
+
+// Task 6
+// Task 7
+// Task 8
+// Task 9
+// Task 10
